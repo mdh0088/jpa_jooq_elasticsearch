ac91e96e5ec9afc06f9e58d9c213a35686b16448
poyi123 <mdh0088@gmail.com>
2023-01-17 13:49:06 +0900

    first commit

---
+++ src/main/java/com/example/japjooqes/JapjooqesApplication.java
@@ -0,0 +1,13 @@
+package com.example.japjooqes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapjooqesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapjooqesApplication.class, args);
+	}
+
+}
